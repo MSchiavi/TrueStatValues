2cd7ef0bdc07557a9bd82dc040aaada31c6abf90
Mahdi <mahdi.kazemizadeh@gmail.com>
2024-08-16 23:24:39 +0330

    Folder removed and Toottips updated

--- Tooltips.lua
+++ Tooltips.lua
@@ -1,4 +1,4 @@
-local name, addon = ...;
+local name, addon = ...
 
 local function RGBPercToHex(r, g, b)
     r = r <= 1 and r >= 0 and r or 0
@@ -9,15 +9,15 @@ end
 
 local statLabelMap = {
     [CR_MASTERY] = function()
-        local primaryTalentTree = GetSpecialization();
+        local primaryTalentTree = GetSpecialization()
         if (primaryTalentTree) then
-            local masterySpell, masterySpell2 = GetSpecializationMasterySpells(primaryTalentTree);
+            local masterySpell, masterySpell2 = GetSpecializationMasterySpells(primaryTalentTree)
             if (masterySpell) then
-                local name = C_Spell.GetSpellName(masterySpell);
-                return name;
+                local name = C_Spell.GetSpellName(masterySpell)
+                return name
             end
         end
-        return nil;
+        return nil
     end,
     [CR_CRIT_SPELL] = function()
         return format(PAPERDOLLFRAME_TOOLTIP_FORMAT, STAT_CRITICAL_STRIKE)
@@ -46,18 +46,18 @@ local patterns = {
 
 function addon.tsv:OnTooltip(ev, tooltip, ...)
     if (addon.tsv.db.global.showStatTooltips) then
-        local tt1 = GameTooltipTextLeft1;
+        local tt1 = GameTooltipTextLeft1
         if (tt1) then
-            local text = tt1:GetText();
+            local text = tt1:GetText()
             if (text) then
                 for statId, labelGenerator in pairs(statLabelMap) do
                     if (statEventMap[statId] == ev) then
-                        local label = labelGenerator();
+                        local label = labelGenerator()
                         if (label) then
                             label = label:gsub("%-", "%%-")
                             local s, e = text:find(label)
                             if (s and s <= 11) then
-                                self:AddTrueStatValuesTooltip(tooltip, statId);
+                                self:AddTrueStatValuesTooltip(tooltip, statId)
                             end
                         end
                     end
@@ -66,30 +66,33 @@ function addon.tsv:OnTooltip(ev, tooltip, ...)
         end
     end
     if (addon.tsv.db.global.showItemTooltips and ev == "OnTooltipSetItem") then
-        local name, link = tooltip:GetItem();
+        local name, link = tooltip:GetItem()
 
         if (name ~= nil and link ~= nil) then
-            local isEquipped = C_Item.IsEquippedItem(name);
-            local itemString = select(3, string.find(link, "|H(.+)|h"));
+            local isEquipped = C_Item.IsEquippedItem(name)
+            local itemString = select(3, string.find(link, "|H(.+)|h"))
             -- print(GetItemInfo(link));
 
             for i = 1, 20, 1 do
-                local textleft = "GameTooltipTextLeft" .. tostring(i);
+                local textleft = "GameTooltipTextLeft" .. tostring(i)
                 if (_G[textleft] and _G[textleft].GetText) then
-                    local text = _G[textleft]:GetText();
+                    local text = _G[textleft]:GetText()
                     if (text and text ~= "") then
                         for statId, pattern in pairs(patterns) do
-                            local amount = string.match(text, pattern);
+                            local amount = string.match(text, pattern)
                             if (amount) then
-                                local s, e = string.find(text, pattern);
-                                local trueAmount = self:GetTrueStatRatingAdded(statId, amount);
-                                local r, g, b = addon.tsv.db.global.fontColor.r, addon.tsv.db.global.fontColor.g,
-                                    addon.tsv.db.global.fontColor.b;
-                                local hexStr = RGBPercToHex(r, g, b);
+                                local s, e = string.find(text, pattern)
+                                local trueAmount = self:GetTrueStatRatingAdded(statId, amount)
+                                local r, g, b =
+                                    addon.tsv.db.global.fontColor.r,
+                                    addon.tsv.db.global.fontColor.g,
+                                    addon.tsv.db.global.fontColor.b
+                                local hexStr = RGBPercToHex(r, g, b)
 
-                                local trueText = text:sub(1, e) .. " |cff" .. hexStr .. "(" .. tostring(trueAmount) ..
-                                    ")|r" .. text:sub(e + 1);
-                                _G[textleft]:SetText(trueText);
+                                local trueText =
+                                    text:sub(1, e) ..
+                                    " |cff" .. hexStr .. "(" .. tostring(trueAmount) .. ")|r" .. text:sub(e + 1)
+                                _G[textleft]:SetText(trueText)
                             end
                         end
                     end
@@ -97,36 +100,36 @@ function addon.tsv:OnTooltip(ev, tooltip, ...)
             end
         end
 
-        _G["FRM"] = tooltip;
+        _G["FRM"] = tooltip
     end
 end
 
 function addon.tsv:AddTrueStatValuesTooltip(tooltip, statId)
-    local statInfo = addon.TrueStatInfo[statId];
-    local pctLabel = (statInfo.bracketPenalty > 0) and ("-" .. tostring(statInfo.bracketPenalty * 100) .. "%") or ("0%");
+    local statInfo = addon.TrueStatInfo[statId]
+    local pctLabel = (statInfo.bracketPenalty > 0) and ("-" .. tostring(statInfo.bracketPenalty * 100) .. "%") or ("0%")
     local barLabel =
-        tostring(statInfo.bracketRating) .. "/" .. tostring(statInfo.bracketMaxRating) .. " [" .. pctLabel ..
-        " penalty]";
-    local r, g, b = addon.tsv.db.global.fontColor.r, addon.tsv.db.global.fontColor.g, addon.tsv.db.global.fontColor.b;
+        tostring(statInfo.bracketRating) ..
+        "/" .. tostring(statInfo.bracketMaxRating) .. " [" .. pctLabel .. " penalty]"
+    local r, g, b = addon.tsv.db.global.fontColor.r, addon.tsv.db.global.fontColor.g, addon.tsv.db.global.fontColor.b
 
     -- barLabel = "|cff0000ff"..barLabel.."|r";
-    local lostRating = (statInfo.baseRating - statInfo.trueRating);
-    lostRating = math.floor(0.005 + 100 * lostRating) / 100;
+    local lostRating = (statInfo.baseRating - statInfo.trueRating)
+    lostRating = math.floor(0.005 + 100 * lostRating) / 100
 
-    tooltip:AddDoubleLine("True Rating:", statInfo.trueRating, r, g, b, r, g, b);
-    tooltip:AddDoubleLine("Lost Rating:", lostRating, r, g, b, r, g, b);
-    GameTooltip_ShowProgressBar(tooltip, 0, statInfo.bracketMaxRating, statInfo.bracketRating, barLabel);
+    tooltip:AddDoubleLine("True Rating:", statInfo.trueRating, r, g, b, r, g, b)
+    tooltip:AddDoubleLine("Lost Rating:", lostRating, r, g, b, r, g, b)
+    GameTooltip_ShowProgressBar(tooltip, 0, statInfo.bracketMaxRating, statInfo.bracketRating, barLabel)
 
-    local frames = tooltip.insertedFrames;
-    local frames_n = #frames;
-    local insertedFrame = frames[frames_n];
+    local frames = tooltip.insertedFrames
+    local frames_n = #frames
+    local insertedFrame = frames[frames_n]
 
     if (insertedFrame and insertedFrame.Bar) then
-        insertedFrame.Bar:SetStatusBarColor(r, g, b, 1);
+        insertedFrame.Bar:SetStatusBarColor(r, g, b, 1)
     end
 
-    tooltip:AddLine("\n");
-    tooltip:Show();
+    tooltip:AddLine("\n")
+    tooltip:Show()
 end
 
 --[[
