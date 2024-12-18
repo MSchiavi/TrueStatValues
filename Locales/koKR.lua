bbc19c7892780a8f5139bce6eb3ffba700355496
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-12-18 10:33:00 +0800

    Add files via upload

---
+++ Locales/koKR.lua
@@ -0,0 +1,18 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("TrueStatValues", "koKR")
+if not L then return end
+
+-- Core
+--L["True Stat Values"] = "True Stat Values"
+--L["Options"] = "Options"
+--L["True Stat Value Settings"] = "True Stat Value Settings"
+--L["Stat Tooltips"] = "Stat Tooltips"
+--L["When checked, displays True Stat Rating information on Secondary-Stat tooltips."] = "When checked, displays True Stat Rating information on Secondary-Stat tooltips."
+--L["Item Tooltips"] = "Item Tooltips"
+--L["When checked, displays True Stat Rating information on Item tooltips."] = "When checked, displays True Stat Rating information on Item tooltips."
+--L["Font Color"] = "Font Color"
+
+-- Tooltips
+--L[" ["] = " ["
+--L[" penalty]"] = " penalty]"
+--L["True Rating:"] = "True Rating:"
+--L["Lost Rating:"] = "Lost Rating:"
---
+++ Locales/zhCN.lua
@@ -0,0 +1,18 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("TrueStatValues", "zhCN")
+if not L then return end
+
+-- Core
+L["True Stat Values"] = "TSV数值统计"
+L["Options"] = "选项"
+L["True Stat Value Settings"] = "TSV设置"
+L["Stat Tooltips"] = "属性提示"
+L["When checked, displays True Stat Rating information on Secondary-Stat tooltips."] = "启用后，鼠标提示在角色界面属性上显示详细数值信息。"
+L["Item Tooltips"] = "装备提示"
+L["When checked, displays True Stat Rating information on Item tooltips."] = "选中后，鼠标提示在部分装备中显示详细数值信息。[饰品]"
+L["Font Color"] = "字体颜色"
+
+-- Tooltips
+L[" ["] = " [当前 "
+L[" penalty]"] = " 递减]"
+L["True Rating:"] = "档位数值："
+L["Lost Rating:"] = "溢出数值："
